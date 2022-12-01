2c46127bccf05fd66b5b2a87fd7c5633cdf082e5
ellie <ellieepprice@gmail.com>
2022-12-01 19:30:31 +0000

    added disclaimer, title, favicon

--- adventify.js
+++ adventify.js
@@ -22,13 +22,13 @@ app.get("/", function (req, res) {
     var link = csv[parseInt(day[2], 10)]
     res.setHeader("Content-Type", "text/html");
     res.write(
-        `<body style=background:#191415;font-family:Arial,Helvetica,sans-serif></body><h1 style= font-size:64px;color:#1DB955;text-align:center>Adventify</h1><div style=text-align:center><a href="${link}"><img src="${pageURL + "/static" + image}"/></a></div> <p style = color:white;text-align:center;font-size:24px>only ${25-day[2]} day(s) until Christmas!</p>`
+        `<head><title>Adventify</title><link rel="icon" type="image/x-icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/96/sparkles_2728.png"></head><body style=background:#191415;font-family:Arial,Helvetica,sans-serif></body><h1 style= font-size:64px;color:#1DB955;text-align:center>Adventify</h1><div style=text-align:center><a href="${link}"><img src="${pageURL + "/static" + image}"/></a></div> <p style = color:white;text-align:center;font-size:24px>only ${25-day[2]} day(s) until Christmas!</p>`
     );
     res.end();
 });
 
 app.get("/about", function (req, res) {
-    res.send("I made this lol");
+    res.send(`<head><title>Adventify</title><link rel="icon" type="image/x-icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/96/sparkles_2728.png"></head><body style="background:#191415;font-family:Arial,Helvetica,sans-serif"><h1 style="font-size:64px;color:#1DB955;text-align:center">Adventify</h1><p style="color:white;text-align:center;font-size:24px">Adventify is not affiliated, associated, authorised, endorsed by, or in any way officially connected with Spotify Technology S.A, or any of its subsidiaries or its affiliates. The official Spotify website can be found at <a href="https://www.spotify.com/">Spotify.com</a>.</p></body>`);
 });
 
 const httpServer = http.createServer(app);
